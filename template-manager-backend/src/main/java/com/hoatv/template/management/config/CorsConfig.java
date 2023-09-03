f5015dcaac17c28d4bb268b5fefc6ab9341c2250
Hoa Tran <tranvanhoactk34@gmail.com>
2023-09-03 23:47:14 +0700

    hvantran/project-management#50 Remove code smell

--- template-manager-backend/src/main/java/com/hoatv/template/management/config/CorsConfig.java
+++ template-manager-backend/src/main/java/com/hoatv/template/management/config/CorsConfig.java
@@ -12,7 +12,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3001", "http://localhost:6088", "http://templateman.local:6088", "http://localhost:8070")
                         .allowedHeaders("*");
             }
         };

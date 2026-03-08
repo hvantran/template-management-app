43d06be2ffe5d80dbb63e7330164aae4eccde930
hvantran <tranvanhoactk34@gmail.com>
2026-03-09 00:40:44 +0700

    [template-manager] Add test environment mock for window._env_
    - Mock window._env_ object in setupTests.js to provide required environment variables
    - Fixes test errors related to missing REACT_APP_PAGES and other env vars
    - Tests still have Babel dependency issues but this resolves the env config problem
    
    Related to hvantran/project-management#190

--- template-manager-ui/src/setupTests.js
+++ template-manager-ui/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock window._env_ for tests
+window._env_ = {
+  REACT_APP_API_URL: 'http://localhost:8080',
+  REACT_APP_PAGES: JSON.stringify([
+    { name: 'Home', path: '/' },
+    { name: 'Test', path: '/test' }
+  ])
+};
